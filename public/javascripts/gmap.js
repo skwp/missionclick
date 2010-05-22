05a823763ddb79180b59ba8974853193ec6b15fd
Pavel Katsev <pkatsev@gmail.com>
2010-05-21 18:40:58 -0700

    cleaned / pimped maps a little bit

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -1,53 +1,80 @@
+// ========================================
+// = Maps events using Google Maps Api v3 =
+// ========================================
+
+// main kick off function for gMaps
 function kickMap() {
     var yelp_api_key = "12YsGWt2MdFgkrgXH4rzQA";
-    
+
+    // setup the map 
     var map = new google.maps.Map($("map_canvas"), {
         zoom: 10,
         center: new google.maps.LatLng(37.77, -122.41),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
     });
-    var bounds = new google.maps.LatLngBounds()
+    var bounds = new google.maps.LatLngBounds();
 
     var temporal_icon = {
         today: '/images/gmap_icons/red_dot.png',
-        tomorrow: '/images/gmap_icons/orange_dot.png',
+        tomorrow: '/images/gmap_icons/yellow_dot.png',
         later: '/images/gmap_icons/teal_dot.png'
     };
 
+    // 
     function addEvents(_events, temporal) {
-        _events.each( function(_event) {
-            addEvent(_event.event, temporal);
-        });
+        _events.each(function(_event) { addEvent(_event.event, temporal); });
     }
+
+    // TODO hack :: not show already mapped venues
+    var mapped_venues = {};
 
     function addEvent(_event, temporal) {
-        var marker = addEventMarker(_event, temporal_icon[temporal]);
-        addEventInfoWindow(_event, marker);
+        var event_id = _event.venue.id;
+        if (!mapped_venues[event_id]) {
+            addEventMarker(_event, temporal_icon[temporal]);
+            mapped_venues[event_id] = 1;
+        }
     }
 
+    // only one instance of infowindow :: jumps around markers
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(infowindow, 'closeclick', function() {
+        map.fitBounds(bounds);      // repan after panning to fit infowindow
+    });
+    
+    // infowindow content template + factory
+    // TODO read from a separate file?
+    var infowindow_template = new Template(
+        '<div class="mapMarker">#{event_title}<hr/><div class="businessinfo">#{event_start_time}</div></div>'
+    );
+    function makeInfowindowContent(_event) {
+        return infowindow_template.evaluate({
+            event_title: _event.title,
+            event_start_time: _event.start_time
+        })
+    }
+
+    // adds event marker for an event to the map
     function addEventMarker(_event, icon) {
         var latlng = new google.maps.LatLng(_event.venue.lat, _event.venue.lng);
-        bounds.extend(latlng);
-        return new google.maps.Marker({
+        var marker = new google.maps.Marker({
               position: latlng,
               map: map,
               icon: icon,
               title: _event.title
         });
-    }
-
-    function addEventInfoWindow(_event, marker) {
-        var infowindow = new google.maps.InfoWindow();
+        bounds.extend(latlng);
         google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(makeInfowindowContent(_event));
             infowindow.open(map, marker);
-            infowindow.setContent(_event.title);
-            lookupYelp(_event.venue);
         });
     }
 
-    addEvents(events.later, 'later');
-    addEvents(events.tomorrow, 'tomorrow');
-    addEvents(events.today, 'today');
+    // add all events
+    ['today', 'tomorrow', 'later'].each(function(key) {
+        addEvents(events[key], key);
+    });
     map.fitBounds(bounds);
 
 }
