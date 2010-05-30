c3ae6e906759b25906c906b9b9f1b77c1cd5acfb
yan <yan@pritzker.ws>
2010-05-30 04:33:52 -0700

    deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+set :application, "missionclick"
+set :repository,  "git@github.com:skwp/missiondog.git"
+set :user, "missionclick"
+set :password, "m1ss10nd0g"
+set :use_sudo, true
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "173.203.214.104" 
+role :app, "173.203.214.104"
+role :db,  "173.203.214.104", :primary => true
+
+after "deploy:setup", "deploy:permissions"
+if ENV["FIRST_TIME"] 
+  before "deploy:migrate", "deploy:setup_db"
+end
+
+# For nginx/passenger
+namespace :deploy do
+  task :permissions do
+    sudo  "chown -R missionclick /u"
+  end
+
+  task :setup_db do
+    run "cd #{release_path} && rake RAILS_ENV=production db:setup"
+  end
+
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
+
