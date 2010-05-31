7664ea07f7a57fa6c2b25a74581fc157812b6901
Pavel Katsev <pkatsev@gmail.com>
2010-05-31 10:52:22 -0700

    made map marking venue-centric

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -22,20 +22,61 @@ function kickMap() {
         later: '/images/gmap_icons/teal_dot.png'
     };
 
-    // 
+    var EventfulVenue = Class.create({
+        initialize: function(venue) { 
+            this.venue = venue;
+            this.temporal_events = {
+                today: [],
+                tomorrow: [],
+                later: []
+            };
+        },
+        addEvent: function(_event, temporal) {
+            this.temporal_events[temporal].push(_event);
+        },
+        getIcon: function() {
+            var temporals = ['today', 'tomorrow', 'later'];
+            for (var i = 0, len = temporals.length; i < len; i++){
+                var temporal = temporals[i];
+                if (this.temporal_events[temporal].length > 0)
+                    return temporal_icon[temporal];
+            }
+        },
+        getEvents: function() {
+            var temporals = ['today', 'tomorrow', 'later'];
+            var results = [];
+            // native loops for max perf :: this is a frequent operation
+            for (var ti = 0, tl = temporals.length; ti<tl; ti++){
+                var events = this.temporal_events[temporals[ti]];
+                for (var ei = 0, el = events.length; ei<el; ei++)
+                    results.push(events[ei]);
+            }
+            return results;
+        }
+    });
+
+    var VenueContainer = {
+        venues: {},
+        get_or_create: function(venue) {
+            var venue_id = venue.id;
+            if (!this.venues[venue_id])
+                this.venues[venue_id] = new EventfulVenue(venue);
+            return this.venues[venue_id];
+        }
+    };
+
     function addEvents(_events, temporal) {
       if(_events)
-        _events.each(function(_event) { addEvent(_event.event, temporal); });
+        _events.each(function(_event) {
+            var _event = _event.event;
+            VenueContainer.get_or_create(_event.venue).addEvent(_event, temporal);
+        });
     }
 
-    // TODO hack :: not show already mapped venues
-    var mapped_venues = {};
-
-    function addEvent(_event, temporal) {
-        var event_id = _event.venue.id;
-        if (!mapped_venues[event_id]) {
-            addEventMarker(_event, temporal_icon[temporal]);
-            mapped_venues[event_id] = 1;
+    function mapVenues() {
+        for (var venue_id in VenueContainer.venues) {
+            var eventfulVenue = VenueContainer.venues[venue_id];
+            addVenueMarker(eventfulVenue);
         }
     }
 
@@ -52,35 +93,44 @@ function kickMap() {
     // infowindow content template + factory
     // TODO read from a separate file?
     var infowindow_template = new Template(
-        '<div class="mapMarker">#{event_title}<hr/><div class="businessinfo">#{event_start_time}</div></div>'
+        '<div class="mapMarker">#{events}</div>'
     );
-    function makeInfowindowContent(_event) {
-        return infowindow_template.evaluate({
-            event_title: _event.title,
-            event_start_time: _event.start_time
-        })
+    var event_entry_template = new Template(
+        '#{event_title}<div class="businessinfo">#{event_start_time}</div>'
+    );
+    function makeInfowindowContent(events) {
+        var entries = events.collect(function(_event){
+            return event_entry_template.evaluate({
+                event_title: _event.title,
+                event_start_time: _event.start_time
+            });
+        }).join('<hr />');
+        return infowindow_template.evaluate({ events: entries });
     }
 
     // adds event marker for an event to the map
-    function addEventMarker(_event, icon) {
-        var latlng = new google.maps.LatLng(_event.venue.lat, _event.venue.lng);
+    function addVenueMarker(eventfulVenue) {
+        var latlng = new google.maps.LatLng(eventfulVenue.venue.lat, eventfulVenue.venue.lng);
         var marker = new google.maps.Marker({
               position: latlng,
               map: map,
-              icon: icon,
-              title: _event.title
+              icon: eventfulVenue.getIcon(),
+              title: eventfulVenue.venue.name
         });
         bounds.extend(latlng);
         google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(makeInfowindowContent(_event));
+            infowindow.setContent(makeInfowindowContent(eventfulVenue.getEvents()));
             infowindow.open(map, marker);
         });
     }
 
-    // add all events
+    // load events into structures
     ['today', 'tomorrow', 'later'].each(function(key) {
         addEvents(events[key], key);
     });
+
+    // map all venues
+    mapVenues();
 
     // commented out by yan - it's a hack but at least it won't jump 
     // to god knows where. the default lat/lng set above is sufficient
