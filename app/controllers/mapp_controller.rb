634a610cca87456a0dbce8870160890b35916bcf
yan <yan@pritzker.ws>
2010-06-03 19:18:19 -0700

    Alpha ordering for venues

--- app/controllers/mapp_controller.rb
+++ app/controllers/mapp_controller.rb
@@ -5,7 +5,7 @@ class MappController < ApplicationController
   def index
     params[:group] ||= 'schedule' #default
     @mapp_date = "Jun 5, 2010"
-    @venues = MappParticipant.find_all_by_mapp_name("Jun 2010", :include => :venue).map(&:venue)
+    @venues = MappParticipant.find_all_by_mapp_name("Jun 2010", :include => :venue, :order => "venues.name asc").map(&:venue)
     @events = Event.scoped(:include => [:venue, :tags]).by_venues(@venues.map(&:id)).scoped(:conditions => ["start_time >= ? and (finish_time <= ? or finish_time is null)", DateTime.parse("Jun 5, 2010 10am PDT"), DateTime.parse("Jun 6, 2010 10am PDT")])
 
     if params[:group] == 'starred'
